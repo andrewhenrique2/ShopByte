e355489c7a0c7dd765c0461da43a7223ccd8c682
andrewhenrique2 <andrewloly06@gmail.com>
2024-07-25 22:00:13 -0300

    Add mobile media

--- src/pages/produto/[id].tsx
+++ src/pages/produto/[id].tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import { FaStar, FaCalendar, FaBolt, FaCreditCard, FaBarcode, FaPlus, FaInfoCircle, FaCartPlus } from 'react-icons/fa';
 import bannerContainer from '../../../public/bannerContainer.jpg';
 
+// Define o tipo para os dados de parcelamento
+
 const ItemDetail = ({
   imageSrc,
   moreImages,
@@ -90,180 +92,165 @@ const ItemDetail = ({
 
 
   return (
-    <div className="container mx-auto p-8 bg-white max-w-[1600px] mt-14 rounded-md mb-28">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="relative h-[500px] w-full ml-28 mt-8">
-          {/* Imagem principal */}
+    <div className="container mt- mx-auto p-8 bg-white max-w-[1600px] mt-14 rounded-md mb-28">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="relative h-[500px] w-full flex justify-center items-center">
+        {/* Imagem principal */}
+        <div className="relative left-0 md:left-24 h-full w-full max-w-[600px]">
           <Image
             src={mainImage}
             alt={title}
             layout="fill"
             objectFit="contain"
-            className="rounded-md"
+            className="rounded-m"
           />
-
-          {/* Imagens adicionais do produto */}
-          <div className="absolute flex flex-col items-center justify-start bottom-4 right-[+115%] translate-x-[+100%] max-h-[550px] overflow-y-auto  ">
-            {moreImages.map((imgSrc: string, index: number) => (
-              <button
-              onClick={() => setMainImage(imgSrc)} // Atualiza a imagem principal ao clicar
-                key={index}
-                className="relative w-[120px] h-[150px] overflow-hidden mb-2 transition-transform transform hover:scale-105 hover:opacity-80"
-              >
-                <Image
-                  src={imgSrc}
-                  alt={`Imagem adicional ${index + 1}`}
-                  layout="responsive"
-                  width={120}
-                  height={80}
-                  style={{ objectFit: 'contain' }}
-                  className="rounded-md cursor-pointer fade-in"
-                />
-              </button>
-            ))}
-          </div>
-
-          {/* Informações adicionais do produto */}
-          <div className="flex flex-col absolute left-16 top-16">
-            {processor && (
-              <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col height-10">
-                Processador{' '}
-                <span
-                  className={`font-black text-[18px] ${
-                    processor.toLowerCase().includes('ryzen') ? 'text-orange-500' : 'text-blue-500'
-                  }`}
-                >
-                  {processor}
-                </span>
-              </div>
-            )}
-            {memory && (
-              <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col">
-                Memória <span className='text-pink font-black text-[20px]'>{memory}</span>
-              </div>
-            )}
-            {storage && (
-              <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col">
-                Armazenamento <span className='text-cian font-black text-[20px]'>{storage}</span>
-              </div>
-            )}
-          </div>
         </div>
-
-
-                              {/* Parte direita do layout */}
-
-        <div className="flex flex-col space-y-4 text-gray-200 bg-container rounded-md p-4 w-[80%] mx-auto">
-          <Image 
-            src={bannerContainer}
-            alt="Banner de promoção" 
-            className="rounded-[6px] w-full" 
-          />
-
-          <div className="flex flex-col space-y-4 mb-4">
-            <div className="flex w-full mb-4">
-              <div className="flex items-center justify-center border border-0.01 border-black rounded-md rounded-e-none w-full">
-                <div className="text-center">
-                  <h2 className="text-[#e15219] font-black">{isOnPromotion ? 'FÉRIAS MODO ON' : 'OFERTA ESPECIAL'}</h2>
-                  <h3 className="text-card font-black">APROVEITE!</h3>
-                </div>
-              </div>
-              <div className="flex items-center justify-center border border-0.01 border-black rounded-md rounded-s-none space-x-4 w-full">
-              <div className="flex flex-col items-center justify-center">
-      <span className="text-yellow-500 text-[14px] font-bold">Fim da Promoção</span>
-      <span className="flex flex-col items-center justify-center text-white text-[14px]">
-        {timeLeft > 0 ? formatTime(timeLeft) : 'Promoção Encerrada'}
-      </span>
-    </div>
+  
+  {/* Imagens adicionais do produto */}
+  <div className="absolute flex flex-row md:flex-col items-center md:items-start md:justify-start left-0 translate-x-0 md:translate-x-0 max-h-[550px] overflow-y-auto md:overflow-hidden space-x-4 md:space-x-0 space-y-4 md:space-y-4 md:mt-0 mt-44  mt-96 ">
+    {moreImages.map((imgSrc: string, index: number) => (
+      <button
+        onClick={() => setMainImage(imgSrc)} // Atualiza a imagem principal ao clicar
+        key={index}
+        className="relative w-[50px] h-[50px] md:w-[120px] md:h-[150px] overflow-hidden mb-2 transition-transform transform hover:scale-105 hover:opacity-80"
+      >
+        <Image
+          src={imgSrc}
+          alt={`Imagem adicional ${index + 1}`}
+          layout="responsive"
+          width={120}
+          height={80}
+          style={{ objectFit: 'contain' }}
+          className="rounded-md cursor-pointer"
+        />
+      </button>
+    ))}
+  </div>
+  
+        {/* Informações adicionais do produto */}
+        <div className="absolute flex flex-col left-[250px]  md:items-start md:left-[250px] top-12 ">
+          {processor && (
+            <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col height-10">
+              Processador{' '}
+              <span
+                className={`font-black text-[18px] ${
+                  processor.toLowerCase().includes('ryzen') ? 'text-orange-500' : 'text-blue-500'
+                }`}
+              >
+                {processor}
+              </span>
+            </div>
+          )}
+          {memory && (
+            <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col">
+              Memória <span className='text-pink font-black text-[20px]'>{memory}</span>
+            </div>
+          )}
+          {storage && (
+            <div className="text-white px-2 font-black bg-black rounded-md text-[16px] mb-1 border-solid flex flex-col">
+              Armazenamento <span className='text-cian font-black text-[20px]'>{storage}</span>
+            </div>
+          )}
+        </div>
+      </div>
+  
+      {/* Parte direita do layout */}
+      <div className="flex flex-col space-y-4 text-gray-200 bg-container rounded-md p-4 w-[100%] mx-auto max-w-[600px]">
+        <Image 
+          src={bannerContainer}
+          alt="Banner de promoção" 
+          className="rounded-[6px] w-full" 
+        />
+  
+        <div className="flex flex-col space-y-4 mb-4">
+          <div className="flex w-full mb-4">
+            <div className="flex items-center justify-center border border-0.01 border-black rounded-md rounded-e-none w-full">
+              <div className="text-center">
+                <h2 className="text-[#e15219] font-black">{isOnPromotion ? 'FÉRIAS MODO ON' : 'OFERTA ESPECIAL'}</h2>
+                <h3 className="text-card font-black">APROVEITE!</h3>
               </div>
             </div>
-
-            <h1 className="text-[18px] font-bold mb-4">{title}</h1>
-
-            <div className="flex items-center gap-4 mb-4">
-              {isNew && (
+            <div className="flex items-center justify-center border border-0.01 border-black rounded-md rounded-s-none space-x-4 w-full">
+              <div className="flex flex-col items-center justify-center">
+                <span className="text-yellow-500 text-[14px] font-bold">Fim da Promoção</span>
+                <span className="flex flex-col items-center justify-center text-white text-[14px]">
+                  {timeLeft > 0 ? formatTime(timeLeft) : 'Promoção Encerrada'}
+                </span>
+              </div>
+            </div>
+          </div>
+  
+          <h1 className="text-[18px] font-bold mb-4">{title}</h1>
+  
+          <div className="flex items-center gap-4 mb-4">
+            {isNew && (
+              <span className="flex items-center gap-2 text-[14px] text-card font-bold">
+                <FaStar className="text-yellow-500 mb-0.5" />
+                NOVO
+              </span>
+            )}
+            {releaseDate && (
+              <span className="flex items-center gap-2 text-[14px] text-card">
+                <FaCalendar className="text-yellow-500 mb-0.5" />
+                {releaseDate}
+              </span>
+            )}
+          </div>
+  
+             {/* Ícones adicionais */}
+             <div className="flex flex-col items-start gap-4 mb-4">
+              <div className="flex gap-4">
                 <span className="flex items-center gap-2 text-[14px] text-card font-bold">
                   <FaStar className="text-yellow-500 mb-0.5" />
                   NOVO
                 </span>
-              )}
-              {releaseDate && (
-                <span className="flex items-center gap-2 text-[14px] text-card">
+                <span className="flex items-center gap-2 text-[14px] text-card font-semibold">
                   <FaCalendar className="text-yellow-500 mb-0.5" />
-                  {releaseDate}
+                  12 meses de garantia
                 </span>
-              )}
-            </div>
-
-
-                          {/*  ícones  aqui */}
-
-
-          <div className="flex flex-col items-start gap-4 mb-4">
-            <div className="flex gap-4">
-
-            <span className="flex items-center gap-2 text-[14px] text-card font-bold ">
-              <FaStar className="text-yellow-500 mb-0.5" />
-              NOVO
-            </span>
-
-            <span className="flex items-center gap-2 text-[14px] text-card font-semibold">
-              <FaCalendar className="text-yellow-500 mb-0.5" />
-              12 meses de garantia
-            </span>
-
-            </div>
-
-            <div className="flex items-center gap-2 text-[14px] text-card font-bold">
-            <span className="flex items-center gap-2 text-[14px] text-card">
-              
-              <FaBolt className="text-green-500 mb-0.5" />
-                PROMOÇÃO
-            </span>
-            <div>
-                <span className="relative group flex items-center gap-2 text-[14px] text-card">
-                  <FaInfoCircle className="text-gray-400 mb-0.5" />
-                  <span className="absolute left-0 invisible group-hover:visible text-sm bg-gray-700 text-white rounded-lg px-2 py-1 whitespace-nowrap">
-                    Disponível no estoque
+              </div>
+  
+              <div className="flex items-center gap-2 text-[14px] text-card font-bold">
+                <span className="flex items-center gap-2 text-[14px] text-card">
+                  <FaBolt className="text-green-500 mb-0.5" />
+                  PROMOÇÃO
+                </span>
+                <div>
+                  <span className="relative group flex items-center gap-2 text-[14px] text-card">
+                    <FaInfoCircle className="text-gray-400 mb-0.5" />
+                    <span className="absolute left-0 invisible group-hover:visible text-sm bg-gray-700 text-white rounded-lg px-2 py-1 whitespace-nowrap">
+                      Disponível no estoque
+                    </span>
                   </span>
-                </span>
-              </div>   
-          </div>
-          </div>
-
-
-         
-
+                </div>   
+              </div>
+            </div>
+  
             <div className="space-y-4 mb-4">
-              <div className="">
               <div className="flex items-center gap-2 text-card">
                 <FaBarcode size={32} className="text-gray-500" />
-                  <span className="text-card text-[17.5px]">
+                <span className="text-card text-[17.5px]">
                   <span className=""> De:</span>
                   <span className="line-through pl-1"> {oldPrice}</span>
                   <span className=""> por:</span>
-                  </span>              
+                </span>              
               </div>
-
+  
               <div className="flex items-center gap-2 text-card">
                 <span className="text-green-400 font-black text-[24px] ml-11"> {formatCurrency(productPrice)}</span>
               </div>
-                <span className="text-[14px] ml-11">À vista com 15% de desconto no boleto ou pix</span>
-              </div>
-              <div className="text-card">
-              </div>
-
-              <div className="text-card mb-8">
+              <span className="text-[14px] ml-11">À vista com 15% de desconto no boleto ou pix</span>
+            </div>
+  
+            <div className="text-card mb-8">
               <div className="flex text-center items-center">
-              <FaCreditCard size={32} className="text-gray-500 mr-4" />
+                <FaCreditCard size={32} className="text-gray-500 mr-4" />
                 <span className="text-[20px] font-bold text-orange-500">{formatCurrency(productPrice)}</span>
-                </div>
-                <span className="text-[16px] px-1 ml-11 "> 12x de {formatCurrency(parseFloat(lastInstallmentValue))} sem juros no cartão</span>
-
-                
-
-                                    {/*  PARCELAMENTO E BOTAO DE COMPRA */}
-                <button 
+              </div>
+              <span className="text-[16px] px-1 ml-11"> 12x de {formatCurrency(parseFloat(lastInstallmentValue))} sem juros no cartão</span>
+  
+              {/* Parcelamento e botão de compra */}
+              <button 
                   className="flex items-center gap-2 text-blue-500 font-semibold ml-11 mt-2" 
                   onClick={() => setShowInstallments(!showInstallments)}
                 >
@@ -299,10 +286,8 @@ const ItemDetail = ({
           </div>
         </div>
       </div>
-    </div>
   );
 };
-
  {/*  PROPS PEGANDO DE CARD E ITENS */}
 
 import { GetServerSideProps } from 'next';
